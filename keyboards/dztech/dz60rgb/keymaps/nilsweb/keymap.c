c5d7e85284065cb193815fd664c303b507929dc4
Nils Weber <me@nilsweb.de>
2019-07-26 12:38:29 +0200

    Enabled dynamic macros

--- keyboards/dztech/dz60rgb/keymaps/nilsweb/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/nilsweb/keymap.c
@@ -29,7 +29,11 @@ enum PlatformKeycodes {
 enum CustomKeycodes {
   CK_MNEMONIC = PK__COUNT,
   CK__COUNT,
+  DYNAMIC_MACRO_RANGE,
 };
+
+// This inlcude has to come after DYNAMIC_MACRO_RANGE is defined!
+#include "dynamic_macro.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -43,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[HYPER] = LAYOUT_ANSI(
       KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______,
-      MO(HYPER2), _______, _______, UK_EURO, _______, _______, _______, UK_UUML, _______, UK_OUML, _______, _______, _______, _______,
+      MO(HYPER2), _______, _______, UK_EURO, _______, _______, _______, UK_UUML, _______, UK_OUML, _______, DYN_MACRO_PLAY1, DYN_MACRO_PLAY2, DYN_REC_STOP,
       _______, UK_AUML, UK_ESZT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, _______,
       _______, _______, _______, _______, _______, KC_BTN2, KC_BTN3, _______, _______, _______, _______, _______,
       _______, _______, _______, KC_BTN1, _______, _______, _______, _______
@@ -51,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[HYPER2] = LAYOUT_ANSI(
       _______, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24, _______,
-      _______, KC_P7, KC_P8, KC_P9, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, KC_P7, KC_P8, KC_P9, _______, _______, _______, _______, _______, _______, _______, DYN_REC_START1, DYN_REC_START2, _______,
       _______, KC_P4, KC_P5, KC_P6, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, _______,
       _______, KC_P1, KC_P2, KC_P3, KC_P0, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______
@@ -144,8 +148,13 @@ bool processCustomKeys(uint16_t keycode, keyrecord_t *record)
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
+  // Process mnemonic unicode input (if activated).
   if (qk_ucis_state.in_progress)
     return process_ucis(keycode, record);
+
+  // Process dynamic macros (if activated).
+  if (!process_record_dynamic_macro(keycode, record))
+    return false;
 
   // Let "standard" keys be handled by QMK.
   if (keycode < SAFE_RANGE) return true;
