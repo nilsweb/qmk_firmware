e541f7006d0cb844b45563a9463ff6832f7e8930
Nils Weber <me@nilsweb.de>
2019-07-26 14:49:17 +0200

    Added <=, >= and => macros to HYPER

--- keyboards/dztech/dz60rgb/keymaps/nilsweb/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/nilsweb/keymap.c
@@ -29,6 +29,9 @@ enum PlatformKeycodes {
 
 enum CustomKeycodes {
   CK_MNEMONIC = PK__COUNT,
+  CK_LTH,
+  CK_GTH,
+  CK_FATARR,
   CK__COUNT,
   DYNAMIC_MACRO_RANGE,
 };
@@ -43,14 +46,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
       MO(HYPER), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
       KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC,
-      PK_LSUPER, KC_LALT, PK_LCOMMAND, KC_SPC, PK_RCOMMAND, MO(FUNCTION), MO(KEYBOARD), CK_MNEMONIC
+      PK_LSUPER, KC_LALT, PK_LCOMMAND, KC_SPC, KC_LEAD, MO(FUNCTION), MO(KEYBOARD), CK_MNEMONIC
     ),
 
 	[HYPER] = LAYOUT_ANSI(
       KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______,
       MO(HYPER2), _______, _______, UK_EURO, _______, _______, _______, UK_UUML, _______, UK_OUML, _______, DYN_MACRO_PLAY1, DYN_MACRO_PLAY2, DYN_REC_STOP,
       _______, UK_AUML, UK_ESZT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, _______,
-      _______, _______, _______, _______, _______, KC_BTN2, KC_BTN3, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, KC_BTN2, KC_BTN3, _______, CK_LTH, CK_GTH, _______, _______,
       _______, _______, _______, KC_BTN1, _______, _______, _______, _______
     ),
 
@@ -58,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24, _______,
       _______, KC_P7, KC_P8, KC_P9, _______, _______, _______, _______, _______, _______, _______, DYN_REC_START1, DYN_REC_START2, _______,
       _______, KC_P4, KC_P5, KC_P6, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, _______,
-      _______, KC_P1, KC_P2, KC_P3, KC_P0, _______, _______, _______, _______, _______, _______, _______,
+      _______, KC_P1, KC_P2, KC_P3, KC_P0, _______, _______, _______, _______, CK_FATARR, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
@@ -139,7 +142,18 @@ bool processCustomKeys(uint16_t keycode, keyrecord_t *record)
   switch (keycode) {
     case CK_MNEMONIC:
       qk_ucis_start();
-      // SEND_STRING("Unsupported at the moment...");
+      break;
+    
+    case CK_LTH:
+      SEND_STRING("<=");
+      break;
+
+    case CK_GTH:
+      SEND_STRING(">=");
+      break;
+
+    case CK_FATARR:
+      SEND_STRING("=>");
       break;
   }
 
